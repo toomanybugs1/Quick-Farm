dc59925952aa131643b42aaf4d6efdeaaeda0174
Fernando Vega Catalán <pytness@gmail.com>
2020-08-02 15:57:30 +0200

    Update Main.java
    Converted tabs to spaces

--- src/main/java/io/github/toomanybugs1/QuickFarm/Main.java
+++ src/main/java/io/github/toomanybugs1/QuickFarm/Main.java
@@ -27,155 +27,155 @@ import org.jetbrains.annotations.NotNull;
 
 public final class Main extends JavaPlugin implements Listener {
 
-	List<String> enabledPlayers;
-	mcMMO mcmmo;
+    List<String> enabledPlayers;
+    mcMMO mcmmo;
 
-	@Override
-	public void onEnable() {
+    @Override
+    public void onEnable() {
 
-		Bukkit.getPluginManager().registerEvents(this, this);
+        Bukkit.getPluginManager().registerEvents(this, this);
 
-		this.saveDefaultConfig();
+        this.saveDefaultConfig();
 
-		this.enabledPlayers = this.getConfig().getStringList("players-enabled");
+        this.enabledPlayers = this.getConfig().getStringList("players-enabled");
 
-		this.mcmmo = (mcMMO) Bukkit.getServer().getPluginManager().getPlugin("mcMMO");
+        this.mcmmo = (mcMMO) Bukkit.getServer().getPluginManager().getPlugin("mcMMO");
 
-		if (this.mcmmo == null)
-			getLogger().info("This server does not have mcMMO. Disabling mcMMO features.");
-		else
-			getLogger().info("mcMMO has been detected!");
-	}
+        if (this.mcmmo == null)
+            getLogger().info("This server does not have mcMMO. Disabling mcMMO features.");
+        else
+            getLogger().info("mcMMO has been detected!");
+    }
 
-	@Override
-	public boolean onCommand(@NotNull CommandSender sender, Command cmd, @NotNull String label, String[] args) {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, Command cmd, @NotNull String label, String[] args) {
 
-		if (!cmd.getName().equalsIgnoreCase("togglequickfarm") || args.length != 0)
-			return false;
+        if (!cmd.getName().equalsIgnoreCase("togglequickfarm") || args.length != 0)
+            return false;
 
-		if (sender instanceof Player) {
+        if (sender instanceof Player) {
 
-			this.enabledPlayers = this.getConfig().getStringList("players-enabled");
+            this.enabledPlayers = this.getConfig().getStringList("players-enabled");
 
-			String playerName = sender.getName();
+            String playerName = sender.getName();
 
-			if (this.enabledPlayers.contains(playerName)) {
-				this.enabledPlayers.remove(playerName);
+            if (this.enabledPlayers.contains(playerName)) {
+                this.enabledPlayers.remove(playerName);
 
-				sender.sendMessage(ChatColor.GOLD + "[QuickFarm] "
-					+ ChatColor.DARK_RED + "Quick farming disabled.");
-			} else {
-				this.enabledPlayers.add(playerName);
+                sender.sendMessage(ChatColor.GOLD + "[QuickFarm] "
+                    + ChatColor.DARK_RED + "Quick farming disabled.");
+            } else {
+                this.enabledPlayers.add(playerName);
 
-				sender.sendMessage(ChatColor.GOLD + "[QuickFarm] "
-					+ ChatColor.DARK_GREEN + "Quick farming enabled.");
-			}
+                sender.sendMessage(ChatColor.GOLD + "[QuickFarm] "
+                    + ChatColor.DARK_GREEN + "Quick farming enabled.");
+            }
 
-			this.getConfig().set("players-enabled", this.enabledPlayers);
-			this.saveConfig();
-		} else {
-			sender.sendMessage("Only players can use this command.");
-		}
+            this.getConfig().set("players-enabled", this.enabledPlayers);
+            this.saveConfig();
+        } else {
+            sender.sendMessage("Only players can use this command.");
+        }
 
 
-		return true;
-	}
+        return true;
+    }
 
-	@EventHandler
-	public void onBlockBreak(BlockBreakEvent event) {
-		//some events were passing null blocks (specifically when using super breaker in mcmmo)
-		if (event == null)
-			return;
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        //some events were passing null blocks (specifically when using super breaker in mcmmo)
+        if (event == null)
+            return;
 
-		Player player = event.getPlayer();
+        Player player = event.getPlayer();
 
-		if (this.enabledPlayers.contains(player.getName()))  // Ensure the player has permission to use this plugin.
-			this.tryAutoReplant(event, player);
-	}
+        if (this.enabledPlayers.contains(player.getName()))  // Ensure the player has permission to use this plugin.
+            this.tryAutoReplant(event, player);
+    }
 
-	/**
-	 * Attempts to automatically replant a crop from seeds in the player's inventory.
-	 *
-	 * Fails under any of the following conditions:
-	 * <ul>
-	 *     <li>the block broken wasn't a crop</li>
-	 *     <li>the player doesn't have the corresponding seeds in his/her inventory.</li>
-	 * </ul>
-	 *
-	 * @param event The event containing information about the block that was broken
-	 * @param player The player whose inventory will be searched for an auto-plantable seed.
-	 */
-	private void tryAutoReplant(BlockBreakEvent event, Player player) {
-		Block block = event.getBlock();
-		ItemStack seed = this.getPlantableSeed(block);
-		BlockData blockData = block.getBlockData();
+    /**
+     * Attempts to automatically replant a crop from seeds in the player's inventory.
+     *
+     * Fails under any of the following conditions:
+     * <ul>
+     *     <li>the block broken wasn't a crop</li>
+     *     <li>the player doesn't have the corresponding seeds in his/her inventory.</li>
+     * </ul>
+     *
+     * @param event The event containing information about the block that was broken
+     * @param player The player whose inventory will be searched for an auto-plantable seed.
+     */
+    private void tryAutoReplant(BlockBreakEvent event, Player player) {
+        Block block = event.getBlock();
+        ItemStack seed = this.getPlantableSeed(block);
+        BlockData blockData = block.getBlockData();
 
-		if (seed == null || !(blockData instanceof Ageable))
-			return;
+        if (seed == null || !(blockData instanceof Ageable))
+            return;
 
-		Ageable blockAge = (Ageable) block.getBlockData();
-		PlayerInventory playerInventory = player.getInventory();
+        Ageable blockAge = (Ageable) block.getBlockData();
+        PlayerInventory playerInventory = player.getInventory();
 
-		if (playerInventory.containsAtLeast(seed, 1)) {
-			event.setCancelled(true);
+        if (playerInventory.containsAtLeast(seed, 1)) {
+            event.setCancelled(true);
 
-			// Drop all items that would normally be dropped.
-			Collection<ItemStack> drops = block.getDrops(new ItemStack(Material.IRON_HOE), player);
+            // Drop all items that would normally be dropped.
+            Collection<ItemStack> drops = block.getDrops(new ItemStack(Material.IRON_HOE), player);
 
-			World playerWorld = player.getWorld();
-			Location blockLocation = block.getLocation();
+            World playerWorld = player.getWorld();
+            Location blockLocation = block.getLocation();
 
-			for (ItemStack drop : drops)
-				playerWorld.dropItemNaturally(blockLocation, drop);
+            for (ItemStack drop : drops)
+                playerWorld.dropItemNaturally(blockLocation, drop);
 
-			// Auto-replant the crop
-			final int previousAge = blockAge.getAge();
-			blockAge.setAge(0);
-			block.setBlockData(blockAge);
+            // Auto-replant the crop
+            final int previousAge = blockAge.getAge();
+            blockAge.setAge(0);
+            block.setBlockData(blockAge);
 
-			// Update the player's inventory to reflect the use of the seed during auto-replanting.
-			final int stackSlot = playerInventory.first(seed.getType());
-			ItemStack seedStack = playerInventory.getItem(stackSlot);
-			
+            // Update the player's inventory to reflect the use of the seed during auto-replanting.
+            final int stackSlot = playerInventory.first(seed.getType());
+            ItemStack seedStack = playerInventory.getItem(stackSlot);
 
-			final int newSeedAmount = seedStack.getAmount() - 1;
-			seedStack.setAmount(newSeedAmount);
 
-			playerInventory.setItem(stackSlot, newSeedAmount > 0 ?
-				seedStack : null
-			);
+            final int newSeedAmount = seedStack.getAmount() - 1;
+            seedStack.setAmount(newSeedAmount);
 
-			// mcmmo should only reward xp if the crop is fully grown
-			if (this.mcmmo != null && previousAge == blockAge.getMaximumAge()) {
-				McMMOPlayer mcPlayer = UserManager.getPlayer(player);
-				ExperienceAPI.addXpFromBlockBySkill(block.getState(), mcPlayer, PrimarySkillType.HERBALISM);
-			}
-		}
+            playerInventory.setItem(stackSlot, newSeedAmount > 0 ?
+                seedStack : null
+            );
 
-	}
+            // mcmmo should only reward xp if the crop is fully grown
+            if (this.mcmmo != null && previousAge == blockAge.getMaximumAge()) {
+                McMMOPlayer mcPlayer = UserManager.getPlayer(player);
+                ExperienceAPI.addXpFromBlockBySkill(block.getState(), mcPlayer, PrimarySkillType.HERBALISM);
+            }
+        }
 
-	/**
-	 * Returns the plantable version of the given block, if one exists.
-	 *
-	 * @param block The block of which to get the plantable version.
-	 * @return The plantable version of the given block if it exists, otherwise null.
-	 */
-	private ItemStack getPlantableSeed(Block block) {
-		// Get the seed corresponding to the block just broken.
-		switch(block.getType()) {
-			case BEETROOTS:
-				return new ItemStack(Material.BEETROOT_SEEDS);
-			case CARROTS:
-				return new ItemStack(Material.CARROT);
-			case POTATOES:
-				return new ItemStack(Material.POTATO);
-			case WHEAT:
-				return new ItemStack(Material.WHEAT_SEEDS);
-			// case NEW_FARM_PLANT:
-			// return new ItemStack(Material.NEW_FARM_SEED);
-			default:  // Indicate no corresponding seed if "block" wasn't a valid crop.
-				return null;
-		}
-	}
+    }
+
+    /**
+     * Returns the plantable version of the given block, if one exists.
+     *
+     * @param block The block of which to get the plantable version.
+     * @return The plantable version of the given block if it exists, otherwise null.
+     */
+    private ItemStack getPlantableSeed(Block block) {
+        // Get the seed corresponding to the block just broken.
+        switch(block.getType()) {
+            case BEETROOTS:
+                return new ItemStack(Material.BEETROOT_SEEDS);
+            case CARROTS:
+                return new ItemStack(Material.CARROT);
+            case POTATOES:
+                return new ItemStack(Material.POTATO);
+            case WHEAT:
+                return new ItemStack(Material.WHEAT_SEEDS);
+            // case NEW_FARM_PLANT:
+            // return new ItemStack(Material.NEW_FARM_SEED);
+            default:  // Indicate no corresponding seed if "block" wasn't a valid crop.
+                return null;
+        }
+    }
 
 }
